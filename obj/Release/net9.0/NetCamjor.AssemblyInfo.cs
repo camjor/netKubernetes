7e3f52fb2879ee5ee4fc77ee43e00254cbbdfacc
Jorge Fonseca <jorgeesteban013@gmail.com>
2025-02-18 11:25:27 -0500

    prueba 3

--- obj/Release/net9.0/NetCamjor.AssemblyInfo.cs
+++ obj/Release/net9.0/NetCamjor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetCamjor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32fbef94be3850e3b08764d52fd960a9fb84866f")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetCamjor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetCamjor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
